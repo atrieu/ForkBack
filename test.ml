9991164c0dc7ff9a73833776489adf70ab89909a
atrieu <alix.trieu@ens-rennes.fr>
2016-02-07 17:15:11 +0100

    no db bullshit

--- test.ml
+++ test.ml
@@ -9,22 +9,15 @@ let print_json req =
   App.json_of_body_exn req >>| fun json ->
   respond (`String ("Received response\n" ^ Ezjsonm.to_string json ^ "\n"))
 
-let string_of_scores scores =
+(*let string_of_scores scores =
   scores.(0).(0)
   
 let print_scores req =
   let scores = scores 0 in
-  App.respond' (`String (string_of_scores scores))
+  App.respond' (`String (string_of_scores scores))*)
   
 let _ =
-  try
-    create_table_if_not_exists ();
-    insert "abc" "lol" 0 0;
     App.empty
     |> post "/" print_json
-    |> get "/" print_scores
+    |> get "/" print_hello
     |> App.run_command
-  with
-  | _ -> App.empty
-	 |> get "/" print_hello
-	 |> App.run_command
