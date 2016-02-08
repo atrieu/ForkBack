23ce0fa6f5e397fb109b4cb8dc07ff03490cb2bf
atrieu <alix.trieu@ens-rennes.fr>
2016-02-08 22:53:40 +0100

    register function

--- test.ml
+++ test.ml
@@ -4,38 +4,32 @@ open Db
 
 let print_hello s req =
   App.respond' (`String ("hello" ^ s))
-       
+
 let print_json req =
-  try
-    App.json_of_body_exn req >>| fun json ->
-    respond (`String ("Received response\n" ^ Ezjsonm.to_string json ^ "\n"))
-  with
-  | _ -> let json = Ezjsonm.from_string "{\"message\": \"Ill-formed json\"}" in
-	   respond' (`Json json)
+  App.string_of_body_exn req >>| fun s ->
+  let json = try Ezjsonm.from_string s with
+	     | _ -> Ezjsonm.from_string "{\"message\": \"Ill-formed json\"}" in
+  respond (`Json json)
 	    
-(*let string_of_scores scores =
-  scores.(0).(0)
-  
-let print_scores req =
-  let scores = scores 0 in
-  App.respond' (`String (string_of_scores scores))*)
+let register req =
+  App.string_of_body_exn req >>| fun s ->
+  let json = try
+      let dict = Ezjsonm.from_string s |> Ezjsonm.value |> Ezjsonm.get_dict in
+      match List.Assoc.find_exn dict "username", List.Assoc.find_exn dict "password" with
+      | `String username, `String password ->
+	 let password = Digest.string password |> Digest.to_hex in
+	 if is_registered username then
+	   Ezjsonm.from_string "{\"status\": 0, \"message\": \"Already registered user\"}"
+	 else let _ = insert username password 0 0 in Ezjsonm.from_string "{\"status\": 1, \"message\": \"Registered\"}"
+      | _ -> Ezjsonm.from_string "{\"status\": 0, \"message\": \"Ill-formed json\"}"
+    with
+    | _ -> Ezjsonm.from_string "{\"status\": 0, \"message\": \"Ill-formed json\"}" in
+  respond (`Json json)
   
 let _ =
-  try
-    let c = connect () in
-    let req = "CREATE TABLE IF NOT EXISTS my_table (
-	       username varchar NOT NULL,
-	       password varchar NOT NULL,
-	       problem integer NOT NULL,
-	       score integer NOT NULL
-	     )" in
-    let res = c#exec req in
-    App.empty
-      |> post "/" print_json
-      |> get "/" (print_hello res#error)
-      |> App.run_command
-  with
-  | _ -> App.empty
-	 |> post "/" print_json
-	 |> get "/" (print_hello " people")
-	 |> App.run_command
+  App.empty
+  |> post "/register" register
+  |> post "/" print_json
+  |> get "/" (print_hello " world")
+  |> App.run_command
+       
