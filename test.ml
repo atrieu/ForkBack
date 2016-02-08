d0b7873bc38ae755f59c72a0dab9b37b3c0c013f
atrieu <alix.trieu@ens-rennes.fr>
2016-02-08 09:55:49 +0100

    try again

--- test.ml
+++ test.ml
@@ -2,13 +2,17 @@ open Core_kernel.Std
 open Opium.Std
 open Db
 
-let print_hello req =
-  App.respond' (`String "hello")
+let print_hello s req =
+  App.respond' (`String ("hello" ^ s))
        
 let print_json req =
-  App.json_of_body_exn req >>| fun json ->
-  respond (`String ("Received response\n" ^ Ezjsonm.to_string json ^ "\n"))
-
+  try
+    App.json_of_body_exn req >>| fun json ->
+    respond (`String ("Received response\n" ^ Ezjsonm.to_string json ^ "\n"))
+  with
+  | _ -> let json = Ezjsonm.from_string "{\"message\": \"Ill-formed json\"}" in
+	   respond' (`Json json)
+	    
 (*let string_of_scores scores =
   scores.(0).(0)
   
@@ -17,7 +21,15 @@ let print_scores req =
   App.respond' (`String (string_of_scores scores))*)
   
 let _ =
+  try
+    let c = connect () in
+    c#finish;
     App.empty
     |> post "/" print_json
-    |> get "/" print_hello
+    |> get "/" (print_hello " world")
     |> App.run_command
+  with
+  | _ -> App.empty
+	 |> post "/" print_json
+	 |> get "/" (print_hello " people")
+	 |> App.run_command
